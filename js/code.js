89224bd4b3ac485280b57c99e5787e089e705ef6
jose ocampo <jose.ocampo@tecmilenio.mx>
2022-02-01 15:00:05 -0600

    commit initial

--- js/code.js
+++ js/code.js
@@ -1,11 +1,66 @@
 function calculariname(){
-    var isurname=document.getElementById('isurname').value;
-    array=['mi proyecto tiene que ver con enviar datos de manera mas rapido por excel','mi trabajo tiene que ver con organizar documentos','mi trabajo tiene que ver con enviar correos','mi proyecto tiene que ver hacer clicks']
-    mix=['https://automatetheboringstuff.com/2e/chapter13/','https://automatetheboringstuff.com/2e/chapter10/','https://automatetheboringstuff.com/2e/chapter18/','https://automatetheboringstuff.com/2e/chapter20/']
+    var isurname=document.getElementById('isurname').value; 
+    
+    file='Reading and writes file'.link(files)
+    org='Organizing files'.link(organizing)
+    webs='Web Scrapting'.link(web)
+    excels='Working with excel Spreadsheets'.link(excel)
+    pdfs='Working with pdf and word documents'.link(pdf)
+    cvss='Working with CVS files and JSON files'.link(cvs)
+    date='Keeping time Scheduling Task and Launching programs'.link(time)
+    emails='Sending Email and Text Messages'.link(email)
+    image='Manipulating images'.link(images)
+    mouse='Controlling a keyboard and Mouse with GUI Automation'.link(gui)
+    
+    
+    array=[
+    'archivo','archivos','folder','folders',
+    'guardar','eliminar','documentos', 'zip','.zip', 'quitar',
+    'web','pagina',
+    'excel','datos','data',
+    'pdf', 'pdfs','word', 'words',
+    'cvs','json', 
+    'fecha','fechas',
+    'correo','mensajes', 'mensaje','correos',
+    'imagen','imagenes',
+    'teclear','mouse','clicks','click'
+    ]
+
+    mix=[
+      file,file,file,file,
+      org,org,org,org,org,org,
+      webs,webs,
+      excels,excels,excels,
+      pdfs,pdfs,pdfs,pdfs,
+      cvss,cvss,
+      date,date,
+      emails,emails,emails,emails,
+      image,image,
+      mouse,mouse,mouse,mouse
+    ]
+    
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
-        if (element == isurname) {
-            document.write('Tú recorrido de aprendizaje será:',mix[i])
+        if (isurname.toLowerCase().includes(element) ) {
+            var resultado=document.getElementById('lazy')
+            resultado.innerHTML='Tus bloques de aprendizaje son <br/>'+mix[i]
         }
     }
 }
+
+
+files='https://automatetheboringstuff.com/2e/chapter9/',
+organizing='https://automatetheboringstuff.com/2e/chapter10/'
+web='https://automatetheboringstuff.com/2e/chapter12/'
+excel='https://automatetheboringstuff.com/2e/chapter13/'
+pdf='https://automatetheboringstuff.com/2e/chapter15/'
+cvs='https://automatetheboringstuff.com/2e/chapter16/'
+time='https://automatetheboringstuff.com/2e/chapter17/'
+email='https://automatetheboringstuff.com/2e/chapter18/'
+images='https://automatetheboringstuff.com/2e/chapter19/'
+gui='https://automatetheboringstuff.com/2e/chapter20/'
+
+
+
+
+
